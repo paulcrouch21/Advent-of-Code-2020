381f830a4e7e9fc153f2ea39c8da0afd50cb383f
paulcrouch99 <paul.crouch99@yahoo.com>
2020-12-08 08:40:55 -0600

    First commit

---
+++ day1/part1.py
@@ -0,0 +1,22 @@
+
+
+
+with open("input.txt", 'r') as f:
+        input = [(int)(line.strip()) for line in f]
+
+c1 = 0
+firstNumber = 0
+secondNumber = 0
+for x in input:
+        c2 = 0
+        while c2 < len(input):
+                firstNumber = input[c1]
+                secondNumber = input[c2]
+                if firstNumber + secondNumber == 2020:
+                        print(firstNumber)
+                        print(secondNumber)
+                        print(firstNumber * secondNumber)
+                        break
+                c2 += 1
+        
+        c1 += 1
