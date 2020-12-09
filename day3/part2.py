84db13d4d2c4d9a3f4858ac7c679e86ac7d722f6
paulcrouch99 <paul.crouch99@yahoo.com>
2020-12-09 06:14:46 -0600

    Removed unneeded comments from old code

--- day3/part2.py
+++ day3/part2.py
@@ -1,14 +1,6 @@
 with open("input.txt", 'r') as f:
         input = [line.strip() for line in f]
 
-#counter = 0
-#for x in input:
-#        if counter == 0 or counter == 1:
-#                input[counter] = x * 1
-#        elif counter != 0 or counter != 1:
-#                input[counter] = x * counter
-#        counter += 1
-
 slopes = [(1,1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
 def traverse(right, down):
